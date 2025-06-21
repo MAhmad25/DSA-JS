56a4b9e6af25faf5834f017a78f7ba49be11058a
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-06-21 12:17:08 +0500

    Add function to move zeroes to the end of an array

---
+++ Move-Zero.js
@@ -0,0 +1,12 @@
+const moveZeroes = function (nums) {
+      let lastNonZero = 0;
+      for (let i = 0; i < nums.length; i++) {
+            if (nums[i] !== 0) {
+                  [nums[lastNonZero], nums[i]] = [nums[i], nums[lastNonZero]];
+                  lastNonZero++;
+            }
+      }
+      return nums;
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
