f11292e4df3ca06d3e7d0573d281bca1e8fd0fd4
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-06-23 10:37:43 +0500

    Added 2 Pointer Method

--- MoveNegativeNumbertoLeft.js
+++ MoveNegativeNumbertoLeft.js
@@ -1,5 +1,20 @@
-const randomArr = [3, 4, 5, -1, 32, -5, 6, -11];
+const arr = [3, 4, 5, -1, 32, -5, 6, -11];
+// Method 1:  By Sorting
 const negativeMoveLeft = (arr) => {
       return arr.sort((a, b) => a - b);
 };
-console.log(negativeMoveLeft(randomArr));
+console.log(negativeMoveLeft(arr));
+
+// //  Method 2:  2 Pointer  Approach
+let lowNumber = 0;
+let highNumber = arr.length - 1;
+while (lowNumber != highNumber) {
+      if (arr[lowNumber] < 0) {
+            lowNumber++;
+      } else if (arr[highNumber] > 0) {
+            highNumber--;
+      } else {
+            [arr[lowNumber], arr[highNumber]] = [arr[highNumber], arr[lowNumber]];
+      }
+}
+console.log(arr);
